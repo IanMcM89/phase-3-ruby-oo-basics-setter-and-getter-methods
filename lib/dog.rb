76e97023d68027f1f71509a5b471bd2dd771716b
Ian <ianmcm.89@gmail.com>
2022-01-26 11:17:02 -0500

    commit tree 618315235841bad571b83fe9d4854e083b79328f

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    #name=
+    def name=(name)
+        @name = name
+    end
 
+    #name
+    def name
+        @name
+    end
+
+    #breed=
+    def breed=(breed)
+        @breed = breed
+    end
+
+    #breed
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
+    #name=
+    def name=(name)
+        @name = name
+    end
 
+    #name
+    def name
+        @name
+    end
+
+    #job=
+    def job=(job)
+        @job = job
+    end
+
+    #job
+    def job
+        @job
+    end
 end
